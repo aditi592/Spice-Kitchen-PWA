0f199f966e14a35dd15cd0a0787cb7df55650dfa
Melroy <melroy27@hotmail.com>
2020-07-29 11:12:35 +0530

    login design

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit ,ViewEncapsulation} from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-login',
