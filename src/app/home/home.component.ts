375115d92e54d7ee9330eae981c593a8540396e0
Melroy Rodrigues <rockstarodmel@gmail.com>
2020-08-30 17:15:51 +0530

    home page changes

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { GetDataService } from '../get-data.service';
-import {BestSeller} from '../best-seller';
+import { BestSeller } from '../best-seller';
 
 @Component({
   selector: 'app-home',
@@ -11,31 +11,23 @@ export class HomeComponent implements OnInit {
   panelOpenState = false;
   restDetails: any;
   constructor(private getdata: GetDataService) { }
-
+  foods = [
+    new BestSeller(1, 'Non-Veg', 'https://b.zmtcdn.com/data/dish_photos/396/2dbeec12a33b1d0fd94bb71ed3575396.jpg?fit=around%7C200%3A200&crop=200%3A200%3B%2A%2C%2A', 'Egg Burji', 80, 'Spicy scrambled eggs made with sautéed chopped onions, tomatoes, green chilies and optional spices', '2 Eggs'),
+    new BestSeller(2, 'Non-Veg', 'https://b.zmtcdn.com/data/dish_photos/396/2dbeec12a33b1d0fd94bb71ed3575396.jpg?fit=around%7C200%3A200&crop=200%3A200%3B%2A%2C%2A', 'Egg Burji', 80, 'Spicy scrambled eggs made with sautéed chopped onions, tomatoes, green chilies and optional spices', '2 Eggs'),
+    new BestSeller(3, 'Non-Veg', 'https://b.zmtcdn.com/data/dish_photos/396/2dbeec12a33b1d0fd94bb71ed3575396.jpg?fit=around%7C200%3A200&crop=200%3A200%3B%2A%2C%2A', 'Egg Burji', 80, 'Spicy scrambled eggs made with sautéed chopped onions, tomatoes, green chilies and optional spices', '2 Eggs'),
+    new BestSeller(4, 'Non-Veg', 'https://b.zmtcdn.com/data/dish_photos/396/2dbeec12a33b1d0fd94bb71ed3575396.jpg?fit=around%7C200%3A200&crop=200%3A200%3B%2A%2C%2A', 'Egg Burji', 80, 'Spicy scrambled eggs made with sautéed chopped onions, tomatoes, green chilies and optional spices', '2 Eggs')
+  ];
+  breakfast = [
+    new BestSeller(1, 'Non-Veg', 'https://cdn.cdnparenting.com/articles/2020/02/26144721/PURI-BHAJI-RECIPE.jpg', 'Puri Bhaji', 80, 'Many Indian households prefer puri bhaji and other traditional dishes over cereals for breakfast. Some serve it for lunch along with condiments such as Salad and Pickle.', 'Poori [4 Poori]+Bhaji+Pickle+Salad'),
+    new BestSeller(2, 'Non-Veg', 'https://b.zmtcdn.com/data/dish_photos/396/2dbeec12a33b1d0fd94bb71ed3575396.jpg?fit=around%7C200%3A200&crop=200%3A200%3B%2A%2C%2A', 'Egg Burji', 80, 'Spicy scrambled eggs made with sautéed chopped onions, tomatoes, green chilies and optional spices', '2 Eggs'),
+    new BestSeller(3, 'Non-Veg', 'https://b.zmtcdn.com/data/dish_photos/396/2dbeec12a33b1d0fd94bb71ed3575396.jpg?fit=around%7C200%3A200&crop=200%3A200%3B%2A%2C%2A', 'Egg Burji', 80, 'Spicy scrambled eggs made with sautéed chopped onions, tomatoes, green chilies and optional spices', '2 Eggs'),
+    new BestSeller(4, 'Non-Veg', 'https://b.zmtcdn.com/data/dish_photos/396/2dbeec12a33b1d0fd94bb71ed3575396.jpg?fit=around%7C200%3A200&crop=200%3A200%3B%2A%2C%2A', 'Egg Burji', 80, 'Spicy scrambled eggs made with sautéed chopped onions, tomatoes, green chilies and optional spices', '2 Eggs')
+  ];
   ngOnInit() {
     this.getdata.getRestaurantDetails().subscribe((res) => {
       this.restDetails = res;
       console.log('Parashar Kitchen', res);
     });
   }
-  foods=[
-    new BestSeller(1,"Non-Veg","https://b.zmtcdn.com/data/dish_photos/396/2dbeec12a33b1d0fd94bb71ed3575396.jpg?fit=around%7C200%3A200&crop=200%3A200%3B%2A%2C%2A","Egg Burji",80,"Spicy scrambled eggs made with sautéed chopped onions, tomatoes, green chilies and optional spices","2 Eggs"),
-    new BestSeller(2,"Non-Veg","https://b.zmtcdn.com/data/dish_photos/396/2dbeec12a33b1d0fd94bb71ed3575396.jpg?fit=around%7C200%3A200&crop=200%3A200%3B%2A%2C%2A","Egg Burji",80,"Spicy scrambled eggs made with sautéed chopped onions, tomatoes, green chilies and optional spices","2 Eggs"),
-    new BestSeller(3,"Non-Veg","https://b.zmtcdn.com/data/dish_photos/396/2dbeec12a33b1d0fd94bb71ed3575396.jpg?fit=around%7C200%3A200&crop=200%3A200%3B%2A%2C%2A","Egg Burji",80,"Spicy scrambled eggs made with sautéed chopped onions, tomatoes, green chilies and optional spices","2 Eggs"),
-    new BestSeller(4,"Non-Veg","https://b.zmtcdn.com/data/dish_photos/396/2dbeec12a33b1d0fd94bb71ed3575396.jpg?fit=around%7C200%3A200&crop=200%3A200%3B%2A%2C%2A","Egg Burji",80,"Spicy scrambled eggs made with sautéed chopped onions, tomatoes, green chilies and optional spices","2 Eggs")
-    // new BestSeller(5,"https://b.zmtcdn.com/data/dish_photos/396/2dbeec12a33b1d0fd94bb71ed3575396.jpg?fit=around%7C200%3A200&crop=200%3A200%3B%2A%2C%2A","Egg Burji",80,"Spicy scrambled eggs made with sautéed chopped onions, tomatoes, green chilies and optional spices","2 Eggs"),
-    // new BestSeller(6,"https://b.zmtcdn.com/data/dish_photos/396/2dbeec12a33b1d0fd94bb71ed3575396.jpg?fit=around%7C200%3A200&crop=200%3A200%3B%2A%2C%2A","Egg Burji",80,"Spicy scrambled eggs made with sautéed chopped onions, tomatoes, green chilies and optional spices","2 Eggs"),
-    // new BestSeller(7,"https://b.zmtcdn.com/data/dish_photos/396/2dbeec12a33b1d0fd94bb71ed3575396.jpg?fit=around%7C200%3A200&crop=200%3A200%3B%2A%2C%2A","Egg Burji",80,"Spicy scrambled eggs made with sautéed chopped onions, tomatoes, green chilies and optional spices","2 Eggs"),
-    // new BestSeller(8,"https://b.zmtcdn.com/data/dish_photos/396/2dbeec12a33b1d0fd94bb71ed3575396.jpg?fit=around%7C200%3A200&crop=200%3A200%3B%2A%2C%2A","Egg Burji",80,"Spicy scrambled eggs made with sautéed chopped onions, tomatoes, green chilies and optional spices","2 Eggs")
-  ];
-  breakfast=[
-    new BestSeller(1,"Non-Veg","https://cdn.cdnparenting.com/articles/2020/02/26144721/PURI-BHAJI-RECIPE.jpg","Puri Bhaji",80,"Many Indian households prefer puri bhaji and other traditional dishes over cereals for breakfast. Some serve it for lunch along with condiments such as Salad and Pickle.","Poori [4 Poori]+Bhaji+Pickle+Salad"),
-    new BestSeller(2,"Non-Veg","https://b.zmtcdn.com/data/dish_photos/396/2dbeec12a33b1d0fd94bb71ed3575396.jpg?fit=around%7C200%3A200&crop=200%3A200%3B%2A%2C%2A","Egg Burji",80,"Spicy scrambled eggs made with sautéed chopped onions, tomatoes, green chilies and optional spices","2 Eggs"),
-    new BestSeller(3,"Non-Veg","https://b.zmtcdn.com/data/dish_photos/396/2dbeec12a33b1d0fd94bb71ed3575396.jpg?fit=around%7C200%3A200&crop=200%3A200%3B%2A%2C%2A","Egg Burji",80,"Spicy scrambled eggs made with sautéed chopped onions, tomatoes, green chilies and optional spices","2 Eggs"),
-    new BestSeller(4,"Non-Veg","https://b.zmtcdn.com/data/dish_photos/396/2dbeec12a33b1d0fd94bb71ed3575396.jpg?fit=around%7C200%3A200&crop=200%3A200%3B%2A%2C%2A","Egg Burji",80,"Spicy scrambled eggs made with sautéed chopped onions, tomatoes, green chilies and optional spices","2 Eggs")
-    // new BestSeller(5,"https://b.zmtcdn.com/data/dish_photos/396/2dbeec12a33b1d0fd94bb71ed3575396.jpg?fit=around%7C200%3A200&crop=200%3A200%3B%2A%2C%2A","Egg Burji",80,"Spicy scrambled eggs made with sautéed chopped onions, tomatoes, green chilies and optional spices","2 Eggs"),
-    // new BestSeller(6,"https://b.zmtcdn.com/data/dish_photos/396/2dbeec12a33b1d0fd94bb71ed3575396.jpg?fit=around%7C200%3A200&crop=200%3A200%3B%2A%2C%2A","Egg Burji",80,"Spicy scrambled eggs made with sautéed chopped onions, tomatoes, green chilies and optional spices","2 Eggs"),
-    // new BestSeller(7,"https://b.zmtcdn.com/data/dish_photos/396/2dbeec12a33b1d0fd94bb71ed3575396.jpg?fit=around%7C200%3A200&crop=200%3A200%3B%2A%2C%2A","Egg Burji",80,"Spicy scrambled eggs made with sautéed chopped onions, tomatoes, green chilies and optional spices","2 Eggs"),
-    // new BestSeller(8,"https://b.zmtcdn.com/data/dish_photos/396/2dbeec12a33b1d0fd94bb71ed3575396.jpg?fit=around%7C200%3A200&crop=200%3A200%3B%2A%2C%2A","Egg Burji",80,"Spicy scrambled eggs made with sautéed chopped onions, tomatoes, green chilies and optional spices","2 Eggs")
-  ];
+
 }
