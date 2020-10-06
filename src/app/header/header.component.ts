6c6e0be7ef319df386b729050158acd45cf6f096
Melroy Rodrigues <rockstarodmel@gmail.com>
2020-10-06 20:42:16 +0530

    minor UI

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,17 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { UserService } from '../shared/user.service';
 import { Router } from '@angular/router';
 import * as $ from 'jquery';
+import { faSmileBeam, faSignOutAlt } from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class HeaderComponent implements OnInit {
+  faSmile = faSmileBeam;
+  faSignOut = faSignOutAlt;
+  userDetails: any;
   constructor(private userService: UserService, private router: Router) { }
-  ngOnInit(): void { }
+  ngOnInit() {
+    this.userService.getUserProfile().subscribe(
+      res => {
+        // tslint:disable-next-line: no-string-literal
+        this.userDetails = res['user'];
+      },
+      err => {
+      }
+    );
+  }
   logout() {
     this.userService.deleteToken();
     this.router.navigateByUrl('/login');
+  }
+  ShowMe() {
+    const x = document.getElementById('showDetails');
+    if (x.style.display === 'none') {
+      x.style.display = 'block';
+    } else {
+      x.style.display = 'none';
+    }
   }
 }
